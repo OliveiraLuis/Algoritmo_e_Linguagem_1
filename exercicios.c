3e041d0708c4535816a8ccd8bbc97c1e249a5c13
OliveiraLuis <oliveira1eduardo2luis3@gmail.com>
2018-08-23 16:55:02 -0300

    Adicionado o enunciado de cada exercicio

--- exercicios.c
+++ exercicios.c
@@ -85,12 +85,18 @@ int main()
 }
 
 void linha() {
-    printf("-----------------------\n");
+    printf("------------------------BAZINGA-------------------------\n");
 }
 
 // Retorna o resto da divisão de um numero por 2
 void num01() {
     int num;
+
+    // Enunciado 
+    printf("Enunciado\n");
+    printf("Escreva um programa para ler, calcular e escrever a media aritmetica entre dois numeros.\n");
+    linha();
+
     printf("Digite um numero\n");
     scanf("%d", &num);
     printf("O resto da divisão de %d por 2 eh: %d \n", num, num%2);
@@ -99,6 +105,13 @@ void num01() {
 // Retorna o primeiro numero elevado ao segundo
 void num02() {
     float num1, num2;
+
+    // Enunciado 
+    printf("Enunciado\n");
+    printf("Faca um programa para ler dois valores (x e y), calcular e mostrar x elevado a potência de y.\n");
+    linha();
+
+
     printf("Digite o primeiro valor\n");
     scanf("%f", &num1);
     printf("Digite o segundo valor\n");
@@ -110,6 +123,12 @@ void num02() {
 // Retorna o primeiro numero elevado ao segundo
 void num03() {
     float num1, num2;
+
+    // Enunciado 
+    printf("Enunciado\n");
+    printf("Faca um programa que leia dois numeros reais e em seguida mostre: a soma, o produto, a divisao e a subtracao entre eles.\n");
+    linha();
+
     printf("Digite o primeiro valor\n");
     scanf("%f", &num1);
     printf("Digite o segundo valor\n");
@@ -125,6 +144,12 @@ void num03() {
 // Converte a temperatura de Fº para Cº
 void num04() {
     float fahre;
+
+    // Enunciado 
+    printf("Enunciado\n");
+    printf("Faca um programa no qual o usuario fornece os valores de temperatura em Fahrenheit e o mesmo converte os valores para Celsius Apresente o resultado.\n");
+    linha();
+
     printf("Estcreva a temperatura em Fahrenheit: \n");
     scanf("%f", &fahre);
     printf("O valor de %.2f (f) em Celcius eh: %.2f \n", fahre, (fahre-32)/1.8);
@@ -134,6 +159,12 @@ void num04() {
 void num05() {
     // volume, largura, altura e comprimento
     float v, l, a, c;
+
+    // Enunciado 
+    printf("Enunciado\n");
+    printf("Elaborar um programa que calcule e apresente o volume de uma caixa retangular.\n");
+    linha();
+
     printf("Altura: \n");
     scanf("%f", &a);
     printf("Comprimento: \n");
@@ -148,6 +179,11 @@ void num05() {
 void num06() {
     float prest, valor, taxa, tempo;
 
+    // Enunciado 
+    printf("Enunciado\n");
+    printf("Elaborar um programa para efetuar o cálculo do valor de uma prestação em atraso.\n");
+    linha();
+
     printf("Digite o valor da prestacao \n");
     scanf("%f", &valor);
     printf("Digite o valor da taxa \n");
@@ -161,6 +197,12 @@ void num06() {
 // Calcula a área do quadrado
 void num07() {
     int lado;
+
+    // Enunciado 
+    printf("Enunciado\n");
+    printf("Faca um programa para ler um valor (o lado de um quadrado), calcular e mostrar a respectiva area do quadrado.\n");
+    linha();
+
     printf("Digite o valor do lado do quadrado: \n");
     scanf("%d", &lado);
     printf("O valor da area do quadrado eh: %d\n", lado*lado);
@@ -169,6 +211,12 @@ void num07() {
 // Calcula a área do triângulo
 void num08() {
     int base, altura;
+
+    // Enunciado 
+    printf("Enunciado\n");
+    printf("Escreva um programa que peça os valores da base e da altura e forneça a area do triangulo.\n");
+    linha();
+
     printf("Digite a base: \n");
     scanf("%d", &base);
     printf("Digite a altura: \n");
@@ -180,6 +228,12 @@ void num08() {
 // Calcula o volume do cilindro
 void num09() {
     int altura, raio;
+
+    // Enunciado 
+    printf("Enunciado\n");
+    printf("Faça um programa para ler dois valores (altura e raio de um cilindro), calcular e mostrar o respectivo volume do cilindro. \n");
+    linha();
+
     printf("Digite a raio: \n");
     scanf("%d", &raio);
     printf("Digite a altura: \n");
@@ -191,6 +245,11 @@ void num09() {
 // Calcula a média ponderada de 2 valores
 void num10() {
     float nota1, nota2, peso1, peso2, media;
+
+    // Enunciado 
+    printf("Enunciado\n");
+    printf("Escrever um programa para ler 4 valores, calcular e mostrar a media ponderada obtida por um aluno para 2 notas bimestrais.\n");
+    linha();
 
     printf("Digite a nota 1: \n");
     scanf("%f", &nota1);
